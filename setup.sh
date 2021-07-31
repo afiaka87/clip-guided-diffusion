09fbcf7969d1dc19d906aea659bd5b865271dc2e
Sam Sepiol <>
2021-07-31 10:42:49 -0500

    Fix git ignore

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/openai/CLIP
-git clone https://github.com/openai/guided-diffusion
-pip install -e ./CLIP
-pip install -e ./guided-diffusion
-pip install kornia
