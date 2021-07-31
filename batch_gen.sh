c0742d32fbb9db3772bdae3c3b78c005e223aba9
Sam Sepiol <>
2021-07-31 10:39:08 -0500

    add bash method for generating large batches of images

--- batch_gen.sh
+++ batch_gen.sh
@@ -1,1 +1,11 @@
-cat captions.txt | xargs -n 1 -P 0 -I {} ./cgd_venv/bin/python hq.py "{}";
+#!/bin/bash
+# Usage: ./batch_gen.sh <caption file>
+# Description: Generate one image per line of caption file in current directory.
+# Example: ./batch_gen.sh captions.txt
+# Uses xargs to run the python script in parallel for all lines in caption file.
+# -n 1 means run one at a time.
+# -I {} means replace {} with the line of the caption file.
+
+caption_file=$1
+
+cat $caption_file | xargs -n 1 -I {} ./cgd_venv/bin/python cgd.py '{}';
