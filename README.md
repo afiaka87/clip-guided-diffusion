c703ba8b78f14725e93d6eb0e87d2c3e86bbf2fb
Sam Sepiol <>
2021-08-10 20:01:54 -0500

    Update README. Add new samples

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CLIP Guided Diffusion
+
 From [RiversHaveWings](https://twitter.com/RiversHaveWings).
 
 <a href="https://colab.research.google.com/drive/12a_Wrfi2_gwwAuN3VvMTwVMz9TfqctNj?usp=sharing">
@@ -16,6 +17,9 @@ From [RiversHaveWings](https://twitter.com/RiversHaveWings).
   Modified 512 px Notebook
 </a>
 
+> This code is currently under active development and is subject to frequent changes. Please file an issue if you have any constructive feedback, questions, or issues with the code or colab notebook.
+
+[Gallery](/images/README.md)
 
 ---
 
@@ -28,41 +32,91 @@ source cgd_venv/bin/activate
 (cgd_venv) $ pip install -r requirements.txt
 ```
 
-## Usage:
+## Quick start:
+
 ```sh
-(cgd_venv) $ python cgd.py \
-  --init_image=images/32K_HUHD_Mushroom.png \
-  --image_size 256 \
-  --num_cutouts 8 \
-  --prefix 'outputs' \
-  --batch_size 1 \
-  --clip_guidance_scale 1000 \
-  --tv_weight 150 \
-  --seed 0 \
-  --save_frequency 10 \
-  --device 'cuda' \
-  --diffusion_steps 1000 \
-  --skip_timesteps 500 \
-  --timestep_respacing '1000' \
-  --cutout_power 1.0 \
-  --clip_model 'ViT-B/32' \
-  "32K HUHD Mushroom"
+(cgd_venv) $ python cgd.py "32K HUHD Mushroom"
+Step 999, output 0:
+00%|███████████████| 1000/1000 [00:00<12:30,  1.02it/s]
 ```
+![](/images/32K_HUHD_Mushroom.png?raw=true)
 
-## CLI Output:
-```
-Step 1, output 0:
-00%|██              █▋| 1/250 [??:??<00:00,  ?.??it/s]
-```
-
-## Outputs:
-Generations will be saved in the folder from `--prefix (default:'./outputs')`
+Generations will be saved in the folder from `--prefix` (default:'./outputs')
 
 with the format `{caption}/batch_idx_{j}_iteration_{i}.png`
 
-![](/images/32K_HUHD_Mushroom.png?raw=true)
-> 32K HUHD Mushroom
 
-[More images](/images/README.md)
+### Blending an existing image
 
-> This code is currently under active development and is subject to frequent changes. Please file an issue if you have any constructive feedback, questions, or issues with the code or colab notebook.
+This method will blend an image with the diffusion for a number of steps. 
+You may need to tinker with `--skip_timesteps` to get the best results.
+```sh
+(cgd_venv) $ python cgd.py --init_image=images/32K_HUHD_Mushroom.png --skip_timesteps=500 \
+    "A mushroom in the style of Vincent Van Gogh"
+```
+![](images/a_mushroom_in_the_style_of_vangogh.png?raw=true)
+
+### Image size
+- Default is 256px
+- Available image sizes are `64, 128, 256, 512 pixels (square)`
+- The 512x512 pixel checkpoint **requires a GPU with at least 12GB of VRAM.**
+- clip guidance scale and tv scale will require experimentation for image sizes less than 256px.
+- the 64x64 diffusion checkpoint is challenging to work with and often results in an all-white or all-black image.
+  - This is much less of an issue when using an existing image of some sort.
+```sh
+(cgd_venv) $ python cgd.py \
+    --init_image=images/32K_HUHD_Mushroom.png \
+    --skip_timesteps=500 \
+    --image_size 64 \
+    "8K HUHD Mushroom"
+```
+![](images/32K_HUHD_Mushroom_64.png?raw=true)
+
+```sh
+(cgd_venv) $ python cgd.py --image_size 512 "8K HUHD Mushroom"
+```
+![](images/32K_HUHD_Mushroom_512.png?raw=true)
+
+## Full Usage:
+```sh
+cgd.py [-h] [--image_size IMAGE_SIZE] [--init_image INIT_IMAGE] [--skip_timesteps SKIP_TIMESTEPS] [--num_cutouts NUM_CUTOUTS] [--prefix PREFIX] [--batch_size BATCH_SIZE] [--clip_guidance_scale CLIP_GUIDANCE_SCALE] [--tv_scale TV_SCALE] [--seed SEED] [--save_frequency SAVE_FREQUENCY] [--device DEVICE] [--diffusion_steps DIFFUSION_STEPS]
+              [--timestep_respacing TIMESTEP_RESPACING] [--cutout_power CUTOUT_POWER] [--clip_model CLIP_MODEL] [--class_cond CLASS_COND] [--clip_class_search]
+              prompt
+
+positional arguments:
+  prompt                the prompt
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --image_size IMAGE_SIZE
+                        Diffusion image size. Must be one of [64, 128, 256, 512]. (default: 256)
+  --init_image INIT_IMAGE
+                        Blend an image with diffusion for n steps (default: None)
+  --skip_timesteps SKIP_TIMESTEPS
+                        Number of timesteps to blend image for. CLIP guidance occurs after this. (default: 0)
+  --num_cutouts NUM_CUTOUTS, -cutn NUM_CUTOUTS
+                        Number of randomly cut patches to distort from diffusion. (default: 8)
+  --prefix PREFIX, --output_dir PREFIX
+                        output directory (default: outputs)
+  --batch_size BATCH_SIZE, -bs BATCH_SIZE
+                        the batch size (default: 1)
+  --clip_guidance_scale CLIP_GUIDANCE_SCALE, -cgs CLIP_GUIDANCE_SCALE
+                        Scale for CLIP spherical distance loss. Default value varies depending on image size. (default: 1000)
+  --tv_scale TV_SCALE, -tvs TV_SCALE
+                        Scale for denoising loss. Disabled by default for 64 and 128 (default: 100)
+  --seed SEED           Random number seed (default: 0)
+  --save_frequency SAVE_FREQUENCY, -sf SAVE_FREQUENCY
+                        Save frequency (default: 100)
+  --device DEVICE       device (default: None)
+  --diffusion_steps DIFFUSION_STEPS
+                        Diffusion steps (default: 1000)
+  --timestep_respacing TIMESTEP_RESPACING
+                        Timestep respacing (default: 1000)
+  --cutout_power CUTOUT_POWER, -cutpow CUTOUT_POWER
+                        Cutout size power (default: 1.0)
+  --clip_model CLIP_MODEL
+                        clip model name. Should be one of: [ViT-B/16, ViT-B/32, RN50, RN101, RN50x4, RN50x16] (default: ViT-B/16)
+  --class_cond CLASS_COND
+                        Use class conditional. Required for image sizes other than 256 (default: True)
+  --clip_class_search   Lookup imagenet class with CLIP rather than changing them throughout run. Use `--clip_class_search` on its own to enable. (default: False)
+```
--- cgd.py
+++ cgd.py
@@ -5,20 +5,14 @@ import sys
 from pathlib import Path
 
 from guided_diffusion.nn import checkpoint
-from IPython import display
 from PIL import Image
-from torch.utils.data.dataloader import DataLoader
-from torch.utils.data.dataset import TensorDataset
-from torchvision.transforms.transforms import RandomResizedCrop
 
-from torch_util import (load_tokenized, resize_image, spherical_dist_loss,
-                        tv_loss)
+from torch_util import (IMAGENET_CLASSES, spherical_dist_loss, tv_loss)
 from util import fetch, load_guided_diffusion
 
 sys.path.append("./guided-diffusion")
 import clip
 import torch
-from kornia import augmentation as K
 from torch import clip_, nn
 from torch.nn import functional as F
 from torchvision import transforms
@@ -55,6 +49,25 @@ class MakeCutouts(nn.Module):
             cutouts.append(cutout)
         return self.augs(torch.cat(cutouts))
 
+# (edit by afiaka87)
+# 
+# Compare `prompt` with 1000 imagenet label transcriptions for its classes.
+# `imagenet_classes` list taken from https://github.com/openai/CLIP/notebooks/
+
+def top_imagenet_class(target_text, clip_model=None, device=None, top:int=16):
+    target_text = clip.tokenize(target_text).to(device)
+    text_tokenized = clip.tokenize(IMAGENET_CLASSES).to(device)
+    with torch.no_grad():
+        text_features = clip_model.encode_text(text_tokenized).float()
+        target_text_features = clip_model.encode_text(target_text).float()
+    text_features /= text_features.norm(dim=-1, keepdim=True)
+    target_text_features /= target_text_features.norm(dim=-1, keepdim=True)
+    text_probs = (100.0 * target_text_features @ text_features.T).softmax(dim=-1)
+    sorted_probs, sorted_classes = text_probs.cpu().topk(top, dim=-1, sorted=True)
+    return sorted_classes[0][0]
+
+clip_guidance_scale_dict = {64: 5, 128: 750, 256: 1000, 512: 1500}
+tv_scale_dict = {64: 0, 128: 100, 256: 100, 512: 150}
 """
 [Generate an image from a specified text prompt.]
 """
@@ -63,31 +76,31 @@ def main():
         description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     p.add_argument("prompt", type=str, help="the prompt")
+    p.add_argument("--image_size", type=int, default=256, help="Diffusion image size. Must be one of [64, 128, 256, 512].")
     p.add_argument("--init_image", type=str, help="Blend an image with diffusion for n steps")
     p.add_argument('--skip_timesteps', type=int, default=0, help='Number of timesteps to blend image for. CLIP guidance occurs after this.')
     p.add_argument("--num_cutouts", "-cutn", type=int, default=8, help="Number of randomly cut patches to distort from diffusion.")
     p.add_argument("--prefix", "--output_dir", default="outputs", type=str, help="output directory")
     p.add_argument("--batch_size", "-bs", type=int, default=1, help="the batch size")
-    p.add_argument("--clip_guidance_scale", "-cgs", type=int, default=1000, help="Scale for CLIP's text prompt based loss.")
-    p.add_argument("--tv_weight", "-tvw", type=int, default=100, help="Scale for denoising loss")
+    p.add_argument("--clip_guidance_scale", "-cgs", type=int, default=1000, help="Scale for CLIP spherical distance loss. Default value varies depending on image size.")
+    p.add_argument("--tv_scale", "-tvs", type=int, default=100, help="Scale for denoising loss. Disabled by default for 64 and 128")
     p.add_argument("--seed", type=int, default=0, help="Random number seed")
-    p.add_argument("--save_frequency", "-sf", type=int, default=100, help="Save frequency")
-    p.add_argument("--device", type=str, help="device")
+    p.add_argument("--save_frequency", "-sf", type=int, default=25, help="Save frequency")
+    p.add_argument("--device", type=str, help="device to run on .e.g. cuda:0 or cpu")
     p.add_argument("--diffusion_steps", type=int, default=1000, help="Diffusion steps")
     p.add_argument("--timestep_respacing", type=str, default='1000', help="Timestep respacing")
-    p.add_argument('--cutout_power', '-cutpow', type=float, default=1.0, help='Cutout size power')
-    p.add_argument('--clip_model', type=str, default='ViT-B/16', help='clip model name. Should be one of: [ViT-B/16, ViT-B/32, RN50, RN101, RN50x4, RN50x16]')
-    p.add_argument('--class_cond', type=bool, default=False, help='Class condition')
-    p.add_argument("--image_size", type=int, default=256, help="image size") # TODO - image size only works @ 256; need to fix
+    p.add_argument('--cutout_power', '-cutpow', type=float, default=0.5, help='Cutout size power')
+    p.add_argument('--clip_model', type=str, default='ViT-B/32', help='clip model name. Should be one of: [ViT-B/16, ViT-B/32, RN50, RN101, RN50x4, RN50x16]')
+    p.add_argument('--class_cond', type=bool, default=True, help='Use class conditional. Required for image sizes other than 256')
+    p.add_argument('--clip_class_search', action='store_true', help='Lookup imagenet class with CLIP rather than changing them throughout run. Use `--clip_class_search` on its own to enable. ')
     args = p.parse_args()
 
     # Initialize
-
     prompt = args.prompt 
     clip_guidance_scale = args.clip_guidance_scale
     init_image = args.init_image
     skip_timesteps = args.skip_timesteps
-    image_size = args.image_size # TODO - support other image sizes
+    image_size = args.image_size
     batch_size = args.batch_size
     seed = args.seed
     save_frequency = args.save_frequency
@@ -96,22 +109,13 @@ def main():
     class_cond = args.class_cond
     diffusion_steps = args.diffusion_steps
     timestep_respacing = args.timestep_respacing
-    tv_weight = args.tv_weight
+    tv_scale = args.tv_scale
     clip_model_name = args.clip_model
+    clip_class_search = args.clip_class_search
+    # Disable class randomize from RiversHaveWings if we are passing a class in.
+    randomize_class = not clip_class_search
+
     prefix = args.prefix
-
-    assert clip_model_name in [
-        'ViT-B/16', 'ViT-B/32', 'RN50', 'RN101', 'RN50x4', 'RN50x16'
-    ], 'clip model name should be one of: [ViT-B/16, ViT-B/32, RN50, RN101, RN50x4, RN50x16]'
-    assert timestep_respacing in [
-        '25', '50', '100', '250', '500', '1000', 'ddim25', 'ddim50', 'ddim100', 'ddim250', 'ddim500', 'ddim1000'
-    ], 'timestep_respacing should be one of [25, 50, 100, 250, 500, 1000, ddim25, ddim50, ddim100, ddim250, ddim500, ddim1000]'
-    
-    # Setup
-
-    prompt_as_subdir = re.sub(r'[^\w\s]', '', prompt).replace(' ', '_')[:100] # Remove non-alphabet characters
-    prefix_path = Path(f'{prefix}/{prompt_as_subdir}')
-    os.makedirs(prefix_path, exist_ok=True)
 
     if args.device is None:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -120,29 +124,53 @@ def main():
         print(f"Using user-specified device {args.device}.")
         device = torch.device(args.device)
 
+    assert clip_model_name in [
+        'ViT-B/16', 'ViT-B/32', 'RN50', 'RN101', 'RN50x4', 'RN50x16'
+    ], 'clip model name should be one of: [ViT-B/16, ViT-B/32, RN50, RN101, RN50x4, RN50x16]'
+    assert timestep_respacing in [
+        '25', '50', '100', '250', '500', '1000', 'ddim25', 'ddim50', 'ddim100', 'ddim250', 'ddim500', 'ddim1000'
+    ], 'timestep_respacing should be one of [25, 50, 100, 250, 500, 1000, ddim25, ddim50, ddim100, ddim250, ddim500, ddim1000]'
+    assert image_size in [ 64, 128, 256, 512 ], 'image size should be one of [64, 128, 256, 512]'
+
+    # Setup
+    prompt_as_subdir = re.sub(r'[^\w\s]', '', prompt).replace(' ', '_')[:100] # Remove non-alphabet characters
+    prefix_path = Path(f'{prefix}/{prompt_as_subdir}')
+    os.makedirs(prefix_path, exist_ok=True)
+
+    if image_size == 64 and clip_guidance_scale > 500:
+        clip_guidance_scale = clip_guidance_scale_dict[image_size]
+        tv_scale = tv_scale_dict[image_size]
+        print("CLIP guidance scale and TV scale too high for 64x64 image. Overriding.")
+        print("Setting TV scale to {}".format(tv_scale))
+        print("Setting CLIP guidance scale to {}".format(clip_guidance_scale))
+
+    if args.class_cond:
+        diffusion_path = f'./checkpoints/{image_size}x{image_size}_diffusion.pt'
+    else:
+        diffusion_path = f'checkpoints/256x256_diffusion_uncond.pt'
+
     if seed is not None:
         torch.manual_seed(seed)
-
     # Load guided-diffusion model
-    gd_model, diffusion = load_guided_diffusion(
-        checkpoint_path='./checkpoints/256x256_diffusion_uncond.pt',
-        image_size=image_size,
-        diffusion_steps=diffusion_steps,
-        timestep_respacing=timestep_respacing,
-        device=device,
-        class_cond=class_cond,
-        rescale_timesteps=False,
-    )
+    gd_model, diffusion = load_guided_diffusion(checkpoint_path=diffusion_path, image_size=image_size, diffusion_steps=diffusion_steps, timestep_respacing=timestep_respacing, device=device, class_cond=class_cond)
     # Load CLIP model
-    clip_model = (
-        clip.load(clip_model_name, jit=False)[0].eval().requires_grad_(False).to(device)
-    )
+    clip_model = clip.load(clip_model_name, jit=False)[0].eval().requires_grad_(False).to(device)
     clip_size = clip_model.visual.input_resolution
     # Normalize applied to images before going into the CLIP model
-    normalize = transforms.Normalize(
-        mean=[0.48145466, 0.4578275, 0.40821073],
-        std=[0.26862954, 0.26130258, 0.27577711],
-    )
+    normalize = transforms.Normalize( mean=[0.48145466, 0.4578275, 0.40821073], std=[0.26862954, 0.26130258, 0.27577711])
+
+    # Random or CLIP-selected imagenet class.
+    model_kwargs = {}
+    if not clip_class_search:
+        model_kwargs["y"] = torch.zeros([batch_size], device=device, dtype=torch.long)
+        print("Randomizing class as generation occurs.")
+    else:
+        imagenet_class = top_imagenet_class(prompt, clip_model=clip_model, device=device)
+        model_kwargs["y"] = torch.Tensor([imagenet_class]).to(torch.long).to(device)
+        print(f"The prompt: '{prompt}'")
+        print(f"Scored well with the ImageNet class idx: '{imagenet_class.item()}'")
+        print(f"This index roughly maps to the label:")
+        print(IMAGENET_CLASSES[imagenet_class])
     make_cutouts = MakeCutouts(clip_size, num_cutouts, cutout_size_power=cutout_power, augment_list=[])
     # Embed text with CLIP model
     text_embed = clip_model.encode_text(clip.tokenize(prompt).to(device)).float()
@@ -169,7 +197,7 @@ def main():
             dists = spherical_dist_loss(cutout_embeds, text_embed.unsqueeze(0))
             losses = dists.mean(0)
             tv_losses = tv_loss(x_in)
-            loss = losses.sum() * clip_guidance_scale + tv_losses.sum() * tv_weight
+            loss = losses.sum() * clip_guidance_scale + tv_losses.sum() * tv_scale
             return -torch.autograd.grad(loss, x)[0]
 
     if timestep_respacing.startswith("ddim"):
@@ -177,11 +205,6 @@ def main():
     else:
         diffusion_sample_loop = diffusion.p_sample_loop_progressive
  
-    model_kwargs = {}
-#    if class_cond:
-#        classes = torch.randint(low=0, high=1000, size=(batch_size,), device=device)
-#        model_kwargs["y"] = classes
-
     samples = diffusion_sample_loop(
         gd_model,
         (batch_size, 3, image_size, image_size),
@@ -191,6 +214,7 @@ def main():
         progress=True,
         skip_timesteps=skip_timesteps,
         init_image=init,
+        randomize_class=randomize_class,
     )
 
     print(f"Attempting to generate the caption: '{prompt}'")
@@ -198,7 +222,7 @@ def main():
     print(f"Using {num_cutouts} cutouts.")
     print(f"Using {timestep_respacing} iterations.")
     print(f"Using {clip_guidance_scale} for text clip guidance scale.")
-    print(f"Using {tv_weight} for denoising loss.")
+    print(f"Using {tv_scale} for denoising loss.")
     try:
         current_timestep = diffusion.num_timesteps - 1
         for step, sample in enumerate(samples):
