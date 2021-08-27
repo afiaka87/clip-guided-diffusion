268318c1d5c48de1d0abce9a236f8f1d7ae8787a
Sam Sepiol <>
2021-08-27 11:26:05 -0500

    Imagenet top N works for generic prompt/s

--- cgd/clip_util.py
+++ cgd/clip_util.py
@@ -19,21 +19,15 @@ CLIP_NORMALIZE = tvt.Normalize(mean=[0.48145466, 0.4578275, 0.40821073], std=[
 @lru_cache(maxsize=1)
 def load_clip(model_name='ViT-B/32', device="cuda"):
     if device == "cpu":
-        clip_model = clip.load(model_name, jit=False)[
-            0].eval().to(device=device).float()
+        clip_model = clip.load(model_name, jit=False)[0].eval().to(device=device).float()
+        clip_size = clip_model.visual.input_resolution
+        return clip_model, clip_size
     elif "cuda" in device:
-        clip_model = clip.load(model_name, jit=False)[
-            0].eval().requires_grad_(False).to(device=device)
+        clip_model = clip.load(model_name, jit=False)[0].eval().requires_grad_(False).to(device)
+        clip_size = clip_model.visual.input_resolution
+        return clip_model, clip_size
     else:
         raise ValueError("Invalid or unspecified device: {}".format(device))
-    clip_size = clip_model.visual.input_resolution
-    return clip_model, clip_size
-
-
-def clip_encode_text(model_name, text, device='cuda', truncate: bool = True):
-    clip_model, _ = load_clip(model_name=model_name, device=device)
-    return clip_model.encode_text(clip.tokenize(text, truncate=truncate).to(device))
-
 
 class MakeCutouts(th.nn.Module):
     def __init__(self, cut_size: int, num_cutouts: int, cutout_size_power: float = 1.0, augment_list: list = []):
@@ -65,34 +59,15 @@ class MakeCutouts(th.nn.Module):
         return self.augs(th.cat(cutouts))
 
 
-def imagenet_top_n(prompt: str = '', prompt_min: str = '', min_weight: float = 0.1, device: str = 'cuda', n: int = len(IMAGENET_CLASSES), clip_model_name: str = "ViT-B/32"):
+def imagenet_top_n(text_encodes, device: str = 'cuda', n: int = len(IMAGENET_CLASSES), clip_model_name: str = "ViT-B/32"):
     """
     Returns the top n classes for a given clip model.
     """
     clip_model, _ = load_clip(model_name=clip_model_name, device=device)
-    clip_model = clip_model.to(device)
-    clip_model.eval()
     with th.no_grad():
         imagenet_lbl_tokens = clip.tokenize(IMAGENET_CLASSES).to(device)
-        prompt_tokens = clip.tokenize(prompt).to(device)
-        prompt_min_features = None
-        prompt_min_tokens = None
-        if prompt_min is not None:
-            prompt_min_tokens = clip.tokenize(prompt_min).to(device)
-
         imagenet_features = clip_model.encode_text(imagenet_lbl_tokens).float()
-        prompt_features = clip_model.encode_text(prompt_tokens).float()
-        if prompt_min_tokens is not None:
-            prompt_min_features = clip_model.encode_text(
-                prompt_min_tokens).float()
-            prompt_min_features /= prompt_min_features.norm(
-                dim=-1, keepdim=True)
-
         imagenet_features /= imagenet_features.norm(dim=-1, keepdim=True)
-        prompt_features /= prompt_features.norm(dim=-1, keepdim=True)
-        if prompt_min_features is not None:
-            prompt_features = prompt_features - \
-                (min_weight * prompt_min_features)
-        text_probs = (100.0 * prompt_features @
-                      imagenet_features.T).softmax(dim=-1)
-        return text_probs.cpu().topk(n, dim=-1, sorted=True).indices[0]
+        prompt_features = text_encodes / text_encodes.norm(dim=-1, keepdim=True)
+        text_probs = (100.0 * prompt_features @ imagenet_features.T).softmax(dim=-1)
+        return text_probs.topk(n, dim=-1, sorted=True).indices[0].to(device)
