1b5a5b7543ab9981760b11d0de94ec38449cbc89
Clay M <claymullis@fastmail.com>
2021-09-07 10:49:10 -0500

    Fix parsing image prompts
    Thanks @everythingscomingupmilhouse

--- cgd/cgd.py
+++ cgd/cgd.py
@@ -351,7 +351,7 @@ def main():
         prompts = []
         
     if len(args.image_prompts) > 0:
-        image_prompts = args.prompts.split('|')
+        image_prompts = args.image_prompts.split('|')
     else:
         image_prompts = []
     
