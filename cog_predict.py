5039ebe53cd98ae8351a91c07c434c13b9fecb49
Sam Sepiol <>
2021-10-07 03:02:37 -0500

    Fix cog predict to have optional init image

--- cog_predict.py
+++ cog_predict.py
@@ -9,9 +9,9 @@ from pathlib import Path
 class ClipGuidedDiffusionPredictor(cog.Predictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
-        prefix_path = Path("./cog_output")
+        prefix_path = Path(os.path.realpath("./cog_output"))
         prefix_path.mkdir(exist_ok=True)     
-        self.prefix_path = prefix_path
+        self.prefix_path = Path(prefix_path)
         # theres no need to set the following to class members, the predict function uses their cached values.
         if not os.path.exists(os.path.expanduser("~/.cache/clip-guided-diffusion/128x128_diffusion.pt")):
             _ = script_util.download_guided_diffusion(image_size=128, checkpoints_dir=script_util.CACHE_PATH, class_cond=True)
@@ -20,13 +20,15 @@ class ClipGuidedDiffusionPredictor(cog.Predictor):
 
     @cog.input("prompt", type=str, help="a caption to visualize")
     @cog.input("respace", type=str, help="Number of timesteps", default="ddim100", options=["25", "50", "100", "200", "250", "500", "1000", "ddim25", "ddim50", "ddim100", "ddim200", "ddim250", "ddim500", "ddim1000"])
-    @cog.input("init_image", type=cog.Path, help="an image to blend with diffusion before clip guidance begins. Uses half as many timesteps.")
-    def predict(self, prompt: str, respace: str, init_image: cog.Path):
+    @cog.input("init_image", type=cog.Path, help="an image to blend with diffusion before clip guidance begins. Uses half as many timesteps.", default=None)
+    def predict(self, prompt: str, respace: str, init_image: cog.Path = None):
         # this could feasibly be a parameter, but it's a highly confusing one. Using half works well enough.
         timesteps_to_skip = int(respace.replace("ddim", "")) // 2 if len(str(init_image)) > 0 else 0
+        use_magnitude = True if int(respace.replace("ddim", "")) < 50 else False
+        use_augmentations = True if int(respace.replace("ddim", "")) < 50 else False
         cgd_generator = clip_guided_diffusion(
             prompts=[prompt],
-            init_image=str(init_image),
+            init_image=init_image,
             skip_timesteps=timesteps_to_skip,
             timestep_respacing=respace,
             save_frequency=1,
@@ -35,15 +37,15 @@ class ClipGuidedDiffusionPredictor(cog.Predictor):
             class_cond=True, # fixed to checkpoint
             clip_model_name="ViT-B/32", # changing works, but will break the cache
             randomize_class=True, # only works with class conditioned checkpoints
-            cutout_power=0.5,
+            cutout_power=1.0,
             num_cutouts=32,
             device="cuda",
-            prefix_path=os.path.realpath(self.prefix_path),
+            prefix_path=self.prefix_path,
             progress=True,
-            use_augs=True,
-            sat_scale=32,
-            init_scale=0 if len(str(init_image)) > 0 else 1000,
-            use_magnitude=True,
+            use_augs=use_augmentations,
+            use_magnitude=use_magnitude,
+            sat_scale=0,
+            init_scale=0 if init_image is None else 1000,
         )
         for _, batch in enumerate(cgd_generator):
             yield cog.Path(batch[1]) # second element is the image path, first is the batch index if batch_size > 1
