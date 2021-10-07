e1adf5bfa09010ad6e2dacaeb674a074f07eb9a5
Sam Sepiol <>
2021-10-07 00:40:58 -0500

    Clean up augmentations

--- cgd/modules.py
+++ cgd/modules.py
@@ -1,6 +1,10 @@
 import torch as th
 import torch.nn.functional as tf
 import torchvision.transforms as tvt
+import torchvision.transforms.functional as tvtf
+from torchvision.transforms.transforms import RandomAdjustSharpness, RandomVerticalFlip
+
+from cgd.ResizeRight import interp_methods, resize_right
 
 
 class MakeCutouts(th.nn.Module):
@@ -9,9 +13,8 @@ class MakeCutouts(th.nn.Module):
         self.cut_size = cut_size
         self.cutn = num_cutouts
         self.cut_pow = cutout_size_power
-        custom_augs = []
         if use_augs:
-            custom_augs = [
+            self.augs = tvt.Compose([
                 tvt.RandomHorizontalFlip(p=0.5),
                 tvt.Lambda(lambda x: x + th.randn_like(x) * 0.01),
                 tvt.RandomAffine(degrees=15, translate=(0.1, 0.1)),
@@ -20,11 +23,9 @@ class MakeCutouts(th.nn.Module):
                 tvt.Lambda(lambda x: x + th.randn_like(x) * 0.01),
                 tvt.RandomGrayscale(p=0.15),
                 tvt.Lambda(lambda x: x + th.randn_like(x) * 0.01),
-                tvt.ColorJitter(brightness=0.1, contrast=0.1,
-                                saturation=0.1, hue=0.1),
-                tvt.Lambda(lambda x: x + th.randn_like(x) * 0.01),
-            ]  # TODO: test color jitter specifically
-        self.augs = tvt.Compose(custom_augs)
+            ])
+        else:
+            self.augs = tvt.Compose([])
 
     def forward(self, input: th.Tensor):
         side_x, side_y = input.shape[2:4]
@@ -32,11 +33,13 @@ class MakeCutouts(th.nn.Module):
         min_size = min(side_y, side_x, self.cut_size)
         cutouts = []
         for _ in range(self.cutn):
-            size = int(th.rand([])**self.cut_pow *
-                       (max_size - min_size) + min_size)
+            size = int(th.rand([])**self.cut_pow * (max_size - min_size) + min_size)
             offsetx = th.randint(0, side_x - size + 1, ())
             offsety = th.randint(0, side_y - size + 1, ())
-            cutout = input[:, :, offsety:offsety +
-                           size, offsetx:offsetx + size]
-            cutouts.append(tf.adaptive_avg_pool2d(cutout, self.cut_size))
+            cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
+            cutout = resize_right.resize(cutout, out_shape=[self.cut_size, self.cut_size], interp_method=interp_methods.lanczos3, by_convs=True)
+            cutout = self.augs(cutout)
+            # cutout = tf.adaptive_avg_pool2d(cutout, self.cut_size)
+            cutouts.append(cutout)
+    
         return th.cat(cutouts)
